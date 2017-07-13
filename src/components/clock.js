e8be59d8c330adbc8e96362444c56d87d43fbd3e
Sanny Mulyono <smulyono@me.com>
2017-07-13 13:18:28 -0700

    update the clock with actual time

--- src/components/clock.js
+++ src/components/clock.js
@@ -2,19 +2,71 @@ import React from 'react';
 import Tick from './clockTick';
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hour : 0,
+            minutes : 0,
+            seconds : 0,
+            display : ""
+        };
+        this.runtimer = this.runtimer.bind(this);
+        this.timerObj = undefined;
+        this.runtimer();
+    }
+
+    runtimer() {
+        this.timerObj = setInterval(this.tickTimer.bind(this), 1000);
+    }
+
+    stoptimer() {
+        clearInterval(this.timerObj);
+    }
+
+    tickTimer() {
+        let {
+            hour,
+            minutes,
+            seconds
+        } = this.state;
+
+        let current = new Date();
+        hour = Math.round(((current.getHours() > 12 ? current.getHours() - 12 : current.getHours()) / 12) * 360);
+        minutes = Math.round((current.getMinutes() / 60) * 360);
+        seconds = Math.round((current.getSeconds() / 60) * 360);
+
+        this.setState({
+            hour : hour,
+            minutes : minutes,
+            seconds : seconds,
+            display : current.toLocaleTimeString()
+        });
+
+    }
+
+
     render() {
+        const {
+            hour,
+            minutes,
+            seconds,
+            display
+        } = this.state;
         return (
             <div className='clock-container'>
+                <div className='clock-display-label'>
+                    {display}
+                </div>
                 <div className='clock-display'>
                     <Tick 
                         type='hour'
-                        degree='0' />
+                        degree={hour} />
                     <Tick 
                         type='minute'
-                        degree='10' />
+                        degree={minutes} />
                     <Tick 
                         type='second'
-                        degree='50' />
+                        degree={seconds} />
                 </div>
             </div>
         )
